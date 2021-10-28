0e95c0812b77f95982e444f51da6bdc34ed8754e
spittala <spittala@capspayroll.com>
2021-10-28 13:42:34 +0530

    Third Project

---
+++ Demo/src/DayOne/DemoOne.java
@@ -0,0 +1,37 @@
+package DayOne;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DemoOne {
+
+	public static void main(String[] args) throws InterruptedException, MalformedURLException
+	{
+
+		
+		
+		
+		
+		
+
+	}
+
+}
